29ea13c0a74f39ede0cf910a02c101d42cd2cec8
Rickazuo <rick3209@gmail.com>
2023-05-21 18:22:13 -0300

    remove the js archive

--- src/app/components/galleryArt/images.js
+++
@@ -1,9 +0,0 @@
-const images = [];
-
-for (let i = 1; i <= 80; i++) {
-  const imageName = `art${i}`;
-  const imageImport = require(`../../../../public/${imageName}.png`).default;
-  images.push({ id: i, name: imageName, src: imageImport });
-}
-
-export default images;
